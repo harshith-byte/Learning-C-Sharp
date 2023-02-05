192e475c00231fd72ae89006622803e1983d6742
Harshith R <harr7@publicisgroupe.net>
2023-02-05 13:36:07 +0530

    conditional if completed

--- IfCondition/Program.cs
+++ IfCondition/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Ifcondition
+{
+    class Program{
+        static void Main(string []args){
+            int val =50;
+
+            // if-else 
+            if(val==50){
+                Console.WriteLine("the val is 50");
+            }
+            else if(val>=51 && val<=60){
+                Console.WriteLine("the val is in between 51 and 60");
+            }
+            else{
+                Console.WriteLine("The val is something else");
+            }
+
+            // ternary Operator
+
+            Console.WriteLine(val<50 ? "the val is smaller":"the val is > 50");
+        }
+    }
+}
