2bb24f2dde26e1648c86851e2735f7b14bab2062
Harshith R <harr7@publicisgroupe.net>
2023-02-05 16:26:17 +0530

    Exception Completed

---
+++ Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string []args){
+            int x=10000;
+            int y=0;
+            int result;
+            
+            // try catch expressions make error checking easier
+            try{
+                if(x>1000){
+                    throw new ArgumentOutOfRangeException("x","x is greater than 1000");
+                }
+                result = x/y;
+                Console.WriteLine("the result is {0}",result);    
+            }
+            catch (DivideByZeroException e){
+                Console.WriteLine("divided by zero exception");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e){
+                Console.WriteLine("Sorry 1000 is limit");
+                Console.WriteLine(e.Message);    
+            }
+            finally{
+                Console.WriteLine("this code always runs");
+            }
+        }
+    }
+}
