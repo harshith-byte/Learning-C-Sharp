c6dc9da2bfbe7ab31621f8b9e89daa94c801a916
Harshith R <harr7@publicisgroupe.net>
2023-02-05 16:00:01 +0530

    While loop completed

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string []args){
+            
+            string str = "";
+            // basic while loop
+            Console.WriteLine("Basic While loop");
+            while(str!="exit"){
+                str = Console.ReadLine();
+                Console.WriteLine("You entered {0}",str);
+            }
+            Console.WriteLine();
+
+            // do while loop
+            Console.WriteLine("do-While() loop");
+            do{
+                str = Console.ReadLine();
+                Console.WriteLine("You entered {0}",str);
+            }while(str!="exit");
+        }
+    }
+}
