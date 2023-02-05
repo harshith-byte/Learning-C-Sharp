4a5b0625a3931f9e0866892b615aae7538f6f7c0
Harshith R <harr7@publicisgroupe.net>
2023-02-05 16:09:07 +0530

    Break and Continue completed

---
+++ BreakAndContinue/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BreakandContinue
+{
+    class Program
+    {
+        static void Main(string []args){
+            int []values={15,7,12,23,41,28,9,17,36};
+
+            Console.WriteLine("using break and continue statements");
+            foreach(int i in values){
+                // continue - to skip particular iteration of the loop
+                if(i>=20 && i<=29){
+                    continue;
+                }
+
+                // break - to terminate the loop from futher execution
+                Console.WriteLine("the current value is {0}",i);
+                if(i>=40){
+                    break;
+                }
+            }
+        }
+    }
+}
